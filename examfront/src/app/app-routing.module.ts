cb25bada881575cbbfd3e61093959688107e5c32
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2022-09-14 17:23:34 +0530

    new pages

--- examfront/src/app/app-routing.module.ts
+++ examfront/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
@@ -8,6 +10,16 @@ const routes: Routes = [
     component: SignupComponent,
     pathMatch: 'full'
   },
+  {
+    path:'login',
+    component: LoginComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'',
+    component: HomeComponent,
+    pathMatch:'full'  
+  }
 ];
 
 @NgModule({
